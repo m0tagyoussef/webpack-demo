7155510d285d8c7884e81a00bdeec4535edc481b
maaly zourob <maalyzourob@gmail.com>
2023-04-04 15:21:13 +0300

    bundle react

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { MainContainer } from "./Global.Styles";
+import Nav from "./Components/Nav/Nav";
+// import HomeScreen from "./Screen/HomeScreen/HomeScreen";
+import MovieScreen from "./Screen/MovieScreen/MovieScreen";
+
+function App() {
+  return (
+    <MainContainer>
+      <Nav />
+      {/*
+      <HomeScreen />*/}
+      <MovieScreen />
+    </MainContainer>
+  );
+}
+
+export default App;
---
+++ src/Components/ActorCard/ActorCard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ActorImage, CardContainer } from "./ActorCard.Styles";
+import { Link } from "react-router-dom";
+import { InfoText } from "../../Screen/MovieScreen/MovieScreen.Styles";
+
+function ActorCard({ img, name, id }) {
+  return (
+    <CardContainer as={Link} to={""}>
+      <ActorImage src={img} alt={name} />
+      <InfoText margin={"20px"} fontSize={16} fontWeight={700}>
+        Actor Name
+      </InfoText>
+    </CardContainer>
+  );
+}
+
+export default ActorCard;
